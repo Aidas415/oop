f7ea46587a182128b9c495f52f4ea9f0c8fa56fc
Aidas415 <aidasambotas@gmail.com>
2025-04-14 22:02:20 +0300

    index.js-auto.js-car.js

--- auto.js
+++ auto.js
@@ -1,11 +1,17 @@
 import {Car} from './js/Car.js';
 
-const volvo = new Car();
-
-
-
-
-
-
-
+const volvo = new Car('Volvo S40', 'red', 55);
+const zapas = new Car('Zapas', 'white', 35);
+console.log(volvo.intro());
+console.log(zapas.intro());
+zapas.refill(0);
+console.log(zapas);
+console.log(zapas.refill(-5));
+console.log(zapas.refill(3.5));
+console.log(zapas);
+console.log(zapas.refill(null));
+console.log(zapas.refill('pazadais uzpildyti'));
+console.log(zapas.refill(NaN));
+console.log(zapas.refill(Infinity))
+console.log(zapas);
 
--- js/Car.js
+++ js/Car.js
@@ -6,13 +6,35 @@
     refill() - uzpilti degalu, bet ne daugiau nei telpa. 
 */
 export class Car{
-
+    constructor(model, color, tankMax){
+        this.model = model;
+        this.color = color;
+        this.tankMax = tankMax;
+        this.tank = 0;
+        this.engineIsOn = false;
+    }
+    intro() {
+        return `This is ${this.color} ${this.model}.`;
+    }
+    refill(volume) {
+        if (typeof volume !== 'number' || !isFinite(volume)) {
+            return `Error: only numbers allowed`;
+        }
+        if (volume < 0) {
+            return `Refill with ${volume} litres is not allowed.`;
+        }
+        if (this.engineIsOn) {
+            return `Error: to refill a tank, you must turn off engine first.`;
+        }
+        if(this.tankMax - this.tank >= volume) {
+            this.tank += volume;
+        } 
+        else {
+            this.tank = this.tankMax;
+        }    
+    }
+    travel() {
+        
+    }
 }
-
-
-
-
-
-
-
 
