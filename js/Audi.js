25d867f222765705dbbba696d235ed3b47dd4e4d
Aidas415 <aidasambotas@gmail.com>
2025-04-20 23:03:26 +0300

    Audi.js-Volkswagen.js-Renaulkt.js-Automobiliai.js-transportas.js

---
+++ js/Audi.js
@@ -0,0 +1,10 @@
+import { Automobiliai } from "./Automobiliai.js"
+
+export class Audi extends Automobiliai {
+        this.kebulas = 'visureigis';
+        this.sanaudos = 10.1;
+        this.degaluBakas = 85;
+        this.sedimosVietos = 5;
+        this.mase = 2055;
+    
+}
---
+++ js/Automobiliai.js
@@ -0,0 +1,13 @@
+export class Automobiliai {
+    constructor (modelis, gamybosMetai, spalva, kuroTipas, transmisija) {
+        this.modelis = modelis;
+        this.gamybosMetai = gamybosMetai;
+        this.spalva = spalva;
+        this.kuroTipas = kuroTipas;
+        this.transmisija = 'automatine';
+    }
+}
+
+
+
+
